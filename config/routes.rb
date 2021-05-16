cfb6db4771cd73e648e8dc6385530e855bb5a41d
AkshataKarakalli <akshatakarakalli@gmail.com>
2021-05-16 12:03:50 +0530

    postscomments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources  :posts 
   devise_for :users
   get 'home/index'
   root to: "home#index"
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: 'users' do
+    end
 end
